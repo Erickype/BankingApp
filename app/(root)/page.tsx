86c06974de6a3d4ad82429758b13fd77bef49631
Erick Carrasco <erickype@hotmail.com>
2024-05-03 01:13:20 +0000

    Header and TotalBalance components in Home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalancebox from '@/components/TotalBalancebox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Erick' }
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transaction efficiently"
+          />
+          <TotalBalancebox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1024}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
